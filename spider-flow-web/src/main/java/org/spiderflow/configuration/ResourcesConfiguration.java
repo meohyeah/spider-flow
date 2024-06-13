194aaf8d501048c5c4234ffbb2a9f328c49ecd9e
meohyeah <49436362+meohyeah@users.noreply.github.com>
2024-06-13 15:07:36 +0800

    Update ResourcesConfiguration.java

--- spider-flow-web/src/main/java/org/spiderflow/configuration/ResourcesConfiguration.java
+++ spider-flow-web/src/main/java/org/spiderflow/configuration/ResourcesConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ResourcesConfiguration implements WebMvcConfigurer{
 
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
